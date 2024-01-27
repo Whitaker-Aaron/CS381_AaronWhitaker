f5c44c0171aefaac912df49886e4f7c9bc9b4be2
Aaron Whitaker <aaronw690@AaronW5LAPTOP>
2024-01-26 18:20:21 -0800

    Second Push

---
+++ as0/src/as0.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include "raylib-cpp.hpp"
+
+int main(){
+    raylib::Window window(500, 500, "CS381 - Assignment 0");
+    raylib::Text text;
+    bool flag = false, flag2 = false;
+    float animate = 80;
+    int windHeight = 0, windWidth = 0;
+
+    while(!window.ShouldClose()) { 
+        window.BeginDrawing();
+        {
+            windWidth = window.GetWidth(); 
+            windHeight = window.GetHeight();
+
+           
+            
+             if(flag2 == false){
+                window.ClearBackground(RAYWHITE);
+                text.Draw("School appropriate", (windWidth/2 - animate), (windHeight/2 - 20), 20, raylib::Color::Black());
+                //flag = true;
+            }
+
+             else if(flag2 == true){
+                window.ClearBackground(BLACK);
+                text.Draw("School appropriate", (windWidth/2 - animate), (windHeight/2 - 20), 20, raylib::Color::White());
+                //flag = false;
+            }
+
+        if(flag2 == false){
+            animate += 0.1;
+            if(animate >= 140){
+                flag2 = true;
+            }
+        }
+
+        else if(flag2 == true){
+            animate -= 0.1;
+            //std::cout << animate << std::endl;
+            if(animate <= 0.0f){
+                //std::cout << "second condition triggered" << std::endl;
+                flag2 = false;
+            }
+        }
+
+         
+            
+        }
+        window.EndDrawing();
+
+    }
+
+    return 0;
+}
+
+//Three types of architectures: 
+//- State based: Setup state, changes on state update --> OpenGL, DirectX 
+//- Command based: Record and replay --> Vulcan, Metal, WebGPU
+//- Immediate Based: Ask -> immediate, everytime 
