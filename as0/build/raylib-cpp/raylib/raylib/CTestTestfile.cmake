14983470192bcd64b5f1e00b94c20a97e81fc2ac
Aaron Whitaker <89281972+Whitaker-Aaron@users.noreply.github.com>
2024-02-03 11:53:37 -0800

    Delete as0/build/raylib-cpp directory

--- as0/build/raylib-cpp/raylib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/aaronw690/cs381/CS381_AaronWhitaker/as0/raylib-cpp/raylib/src
-# Build directory: /home/aaronw690/cs381/CS381_AaronWhitaker/as0/build/raylib-cpp/raylib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
